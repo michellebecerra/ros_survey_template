f0de47ca8524be080e4a7ac62ae3b1e2b3b30180
Michelle Becerra <michel8@vt.edu>
2017-02-04 18:48:17 -0800

    framework skeleton

---
+++ mongoNode/app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+var mongoose = require('mongoose');
+var fs = require('fs');
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(require('less-middleware')(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', index);
+app.use('/users', users);
+
+mongoose.connect('mongodb://55.55.55.5/mongo');
+// mongoose.model('users', {name: String});
+
+fs.readdirSync(__dirname + '/models').forEach(function(filename){
+	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
+});
+
+app.get('/users', function(req, res){
+	mongoose.model('users').find(function(err, users){
+		res.send(users)
+	});
+});
+
+// app.get('/posts', function(req, res){
+// 	mongoose.model('posts').find({user: req.params.userId}, function(err, posts){
+// 		mongoose.model.populate(posts, {path: 'user'}, function(err, posts){
+// 			res.send(posts)
+// 		});
+// 	});
+// });
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
