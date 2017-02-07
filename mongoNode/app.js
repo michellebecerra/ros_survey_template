af9517e822937ca200750332cb559482cbb27f1a
Michelle Becerra <michel8@vt.edu>
2017-02-06 23:46:26 -0800

    sample survey page

--- mongoNode/app.js
+++ mongoNode/app.js
@@ -1,12 +1,13 @@
-var express = require('express');
+var express = require('express'); //basic
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var bodyParser = require('body-parser'); //handles input values before getting to express
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var survey = require('./routes/survey');
 
 var mongoose = require('mongoose');
 var fs = require('fs');
@@ -20,26 +21,39 @@ app.set('view engine', 'hjs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(require('less-middleware')(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+// app.use('/', index); //Directs to welcome page
+// app.use('/users', users);
 
-mongoose.connect('mongodb://55.55.55.5/mongo');
+
+//BLOG TUTORIAL
+
+app.get('/', function(req, res){
+  res.send('Hello World!')
+});
+
+app.use('/survey', survey);
+
+app.post('/quotes', function(req, res){
+  console.log(req.body)
+});
+/*YOUTUBE TUTORIAL
+// mongoose.connect('mongodb://55.55.55.5/mongo');
 // mongoose.model('users', {name: String});
 
-fs.readdirSync(__dirname + '/models').forEach(function(filename){
-	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
-});
+// fs.readdirSync(__dirname + '/models').forEach(function(filename){
+// 	if(~filename.indexOf('.js')) require(__dirname + '/models/' + filename)
+// });
 
-app.get('/users', function(req, res){
-	mongoose.model('users').find(function(err, users){
-		res.send(users)
-	});
-});
+// app.get('/users', function(req, res){
+// 	mongoose.model('users').find(function(err, users){
+// 		res.send(users)
+// 	});
+// });
 
 // app.get('/posts', function(req, res){
 // 	mongoose.model('posts').find({user: req.params.userId}, function(err, posts){
@@ -48,6 +62,7 @@ app.get('/users', function(req, res){
 // 		});
 // 	});
 // });
+*/
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
